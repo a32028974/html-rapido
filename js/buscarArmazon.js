bfaeeddc33756607339151feea272be21f7aa732
a32028974 <a32028974@gmail.com>
2025-07-05 11:19:04 -0300

    Update buscarArmazon.js

--- js/buscarArmazon.js
+++ js/buscarArmazon.js
@@ -1,25 +1,25 @@
 import { API_URL } from './api.js';
 
-export async function buscarArmazonPorNumero(numeroArmazon, armazonDetalle, precioArmazon, spinner) {
-  try {
-    if (spinner) spinner.style.display = "block";
+export async function buscarArmazonPorNumero(numeroArmazon, armazonDetalle, spinner) {
+  const precioArmazon = document.getElementById("precio_armazon");
 
+  try {
+    spinner.style.display = "block";
     const res = await fetch(`${API_URL}?buscarArmazon=${numeroArmazon.value.trim()}`);
     const data = await res.json();
 
     if (data.modelo) {
       armazonDetalle.value = data.modelo;
-      precioArmazon.value = data.precio || "";  // se llena el precio si viene
+      precioArmazon.value = data.precio;
     } else {
       armazonDetalle.value = "No encontrado";
       precioArmazon.value = "";
     }
-
   } catch (err) {
     console.error("Error buscando armazón:", err);
     armazonDetalle.value = "Error";
     precioArmazon.value = "";
   } finally {
-    if (spinner) spinner.style.display = "none";
+    spinner.style.display = "none";
   }
 }
