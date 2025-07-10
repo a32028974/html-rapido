f1d88c3411ac50ae201feedc7c022ed3e7135416
a32028974 <a32028974@gmail.com>
2025-07-10 19:13:58 -0300

    Update fechaHoy.js

--- js/fechaHoy.js
+++ js/fechaHoy.js
@@ -1,5 +1,5 @@
-export function cargarFechaHoy() {
+document.addEventListener("DOMContentLoaded", () => {
   const hoy = new Date();
   const fechaFormateada = hoy.toLocaleDateString("es-AR");
   document.getElementById("fecha").value = fechaFormateada;
-}
+});
