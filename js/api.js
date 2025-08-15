baafeff416360c6e9d021e5f2bfa3179ceff84cf
a32028974 <a32028974@gmail.com>
2025-08-15 18:26:17 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,2 +1,2 @@
 // api.js
-export const API_URL = "https://script.google.com/macros/s/AKfycbxoizon8d6ofxylcDqMjMAd1SCfJNaR-J4arYJI1r6CbVJjneKdFqxktHgJSNPXh4p_tQ/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbxQbVQGAtZqCWXRash0wLqvV9nbFA9ZdA4Kv-5cR0JcqGp5bOEiPPWFXB_7a5IB9txClw/exec";
